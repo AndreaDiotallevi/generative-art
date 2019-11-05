9513085fbf96e19279de12af97ea6b96a12fb24d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-05 19:06:14 +0000

    Refactor moonlight sketch

--- moonlight/sketch.js
+++ moonlight/sketch.js
@@ -4,22 +4,65 @@
 // Moonlight
 ///////////////////////////////////////////////////////////////////////////////
 
-let w = 900;
-let r = w / 2;
-let t = 0;
-let a, b, c, d;
+// Set filename
+let filename = "moonlight";
 
-setup = _ => {
-	createCanvas(w, w);
+// Set canvas size
+let width = 600;
+let height = 600;
+
+// Set noise seed value
+let seed = 1;
+
+// Set time variables
+let time = 0;
+let timeIncrement = 1;
+let timeLimit = 1000;
+
+// Set frame rate
+let fps = 30;
+
+// Set moon radius and declare noise radius
+let moonRadius = width / 4;
+let noiseRadius;
+
+// Declare angles
+let angle1, angle2;
+
+// Declare points coordinates
+let x1, y1, x2, y2;
+
+function setup() {
+	createCanvas(width, height);
 	background(0, 6, 60);
 	stroke(255, 80);
+	noiseSeed(seed);
+	frameRate(fps);
 }
 
-draw = _ => {
-	a = noise(t + 9) * r;
-  b = noise(w) * r;
-	c = noise(t) * 6;
-  d = noise(t + 60) * 6;
-	line(cos(c) * a + r, sin(c) * a + r, cos(d) * b + r, sin(d) * b + r);
-  t++;
+function draw() {
+	translate(width / 2, height / 2);
+	updateCoordinates();
+	line(x1, y1, x2, y2);
+	time += timeIncrement;
+	saveFinalImage();
+}
+
+function updateCoordinates() {
+	noiseRadius = noise(time + 9) * width / 2;
+	angle1 = noise(time) * 6;
+  angle2 = noise(time + 60) * 6;
+	x1 = cos(angle1) * noiseRadius;
+	y1 = sin(angle1) * noiseRadius;
+	x2 = cos(angle2) * moonRadius;
+	y2 = sin(angle2) * moonRadius;
+}
+
+function saveFinalImage() {
+	if (time == timeLimit) {
+		noLoop();
+		outputFilename = `${filename}-${seed}.png`;
+		save(outputFilename);
+		console.log(outputFilename);
+	}
 }
